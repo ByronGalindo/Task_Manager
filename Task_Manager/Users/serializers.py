95730e87915df978d7ba1927f2c32239d08c27d5
ByronGalindo <bahegasu2000@gmail.com>
2024-02-01 09:34:52 -0500

    Update Encrypt for password

--- Task_Manager/Users/serializers.py
+++ Task_Manager/Users/serializers.py
@@ -2,11 +2,19 @@ from rest_framework import serializers
 from .models import Users
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    
+    def create(self, validated_data):
+        user = Users.objects.create_user(**validated_data)
+        return user
     class Meta:
         model = Users
         fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'id': {'read_only': True},
+        }
 
+    
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
