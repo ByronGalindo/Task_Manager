65ffb98c109db14ba82ee41646b8fd850450a0f0
ByronGalindo <bahegasu2000@gmail.com>
2024-02-01 08:59:11 -0500

    Implement logic for task manager

--- Task_Manager/ToDo/views.py
+++ Task_Manager/ToDo/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        if serializer.instance.user != self.request.user:
+            # No permitir que el usuario modifique tareas de otros usuarios
+            raise PermissionDenied("No tienes permiso para modificar esta tarea.")
+        serializer.save()
