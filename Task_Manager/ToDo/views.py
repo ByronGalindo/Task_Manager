5a3289e56fced6150ae5b8940f4edb2932d17c27
ByronGalindo <bahegasu2000@gmail.com>
2024-02-01 10:18:22 -0500

    Incorpore filters in urls

--- Task_Manager/ToDo/views.py
+++ Task_Manager/ToDo/views.py
@@ -3,12 +3,20 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.exceptions import PermissionDenied
+from django_filters import rest_framework as filters
 
+
+class TaskFilter(filters.FilterSet):
+    class Meta:
+        model = Task
+        fields = ['status']
 
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = TaskFilter
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
